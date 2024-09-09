2f2185f4a14792418ad2126e70b153e7649e7a5c
ubuntuchair <ralis3333@gmail.com>
2024-09-09 22:44:35 +0300

    Comments

--- A2.cpp
+++ A2.cpp
@@ -6,51 +6,53 @@ int a2() {
     int m, n;
 
     cout << "Ievadiet pirmo naturālo skaitli: ";
-    cin >> m;
+    cin >> m; // Lai lietotajs ievada skaitli
     cout << "Ievadiet otro naturālo skaitli: ";
     cin >> n;
 
-    int m_divider_count = 0;
+    int m_divider_count = 0; // Sāk ar 0, lai varētu saskaitīt dalītāju skaitu
 
-    for (int divider = 1; divider <= m; divider++) {
-        if ((m % divider) == 0) {
+    for (int divider = 1; divider <= m; divider++) { // tiks skaitīts no 1 lidz ievadītajam skaitlim ieskaitot, 
+                                                    // lai pārbaudītu visus iespējamos dalītājus
+        if ((m % divider) == 0) { // tiek pārbaudīts, lai izdalot būs vesels skatlis
             m_divider_count +=1;
         }
     };
 
-    int n_divider_count = 0;
+    int n_divider_count = 0; // Sāk ar 0, lai varētu saskaitīt dalītāju skaitu
 
-    for (int divider = 1; divider <= n; divider++) {
-        
-        if ((n % divider) == 0) {
+    for (int divider = 1; divider <= n; divider++) { // tiks skaitīts no 1 lidz ievadītajam skaitlim ieskaitot, 
+                                                    // lai pārbaudītu visus iespējamos dalītājus
+        if ((n % divider) == 0) { // tiek pārbaudīts, lai izdalot būs vesels skatlis
             n_divider_count +=1;
         }
     };
 
-    int count = m_divider_count - n_divider_count;
+    int count = m_divider_count - n_divider_count; // salīdzina starpību starp abu skaitļu dalītāju skaitu
 
     return count;
 }
 
 int main() {
 
-    bool continue_prog = true;
+    bool continue_prog = true; // nosaka, lai programma izpildītos vismaz vienu reizi
 
     do {
-        int count = a2();
-        std::cout << count << endl;
+        int count = a2(); // izsauc funkciju, kas paprasīs lietotājam naturālus skaitļus un 
+                            // noteiktu dalītāju skatu starpību
+        std::cout << "Atbilde:" << count << endl;
 
         char user_choise;
-        std::cout << "Vēlaties turpināt? (Y/n):";
+        std::cout << "Vēlaties turpināt? (Y/n):"; 
         cin >> user_choise;
 
-        if (user_choise == 'n') {
+        if (user_choise == 'n') { // tiek izvēlēts n un N, jo tas tiek izmantos kā standartilizētas opcijas, kas nozīmē nē
             continue_prog = false;
         } else if (user_choise == 'N') {
             continue_prog = false;
-        }
+        } 
 
-    } while (continue_prog == true);
+    } while (continue_prog == true); // visas citas opcijas nozīmēs jā
     
     return 0;
 }
@@ -60,4 +62,14 @@ Gustavs Krasņikovs, gk24018
 A2. Izveidot programmu, kas apreiķina naturālu skaitļa, ko ievada lietotājs, 
 dalāmo skaitu un salīdzina kuram skaitlim ir lielāks dalāmo skaits, izvadot dalāmo skatļu starpību.
 Programma izveidota: 07.09.2024.
+*/
+
+/*
+    Ievads      Programmas vēlamā    Rezultāts C++
+                    reakcija
+
+     9 6              -1                   +
+     12 3              4                   +
+     54 0              8                   +
+     0 0               0                   +
 */
--- A2.py
+++ A2.py
@@ -2,17 +2,23 @@ m = int(input('Ievadiet pirmo naturālo skaitli: '))
 n = int(input('Ievadiet otro naturālo skaitli: '))
 
 
-m_diveder_count = 0
+m_diveder_count = 0 # Sāk ar 0, lai varētu saskaitīt dalītāju skaitu
 
-for divider in range(1, m + 1):
-    if m % divider == 0:
-        m_diveder_count += 1
+if m > 0: # pārbaudīts lai ievadītais skaitlis ir naturāls
+    for divider in range(1, m + 1): # tiks skaitīts no 1 lidz ievadītajam skaitlim ieskaitot, 
+                                # lai pārbaudītu visus iespējamos dalītājus
+        if m % divider == 0: # rezultātu salīdzina ar 0
+                            # lai uzinātu ka reizinās ar konkrēto skaitli
+            m_diveder_count += 1
 
-n_diveder_count = 0
+n_diveder_count = 0 # Sāk ar 0, lai varētu saskaitīt dalītāju skaitu
 
-for divider in range(1, m + 1):
-    if n % divider == 0:
-        n_diveder_count += 1
+if n > 0: # pārbaudīts lai ievadītais skaitlis ir naturāls
+    for divider in range(1, m + 1): # tiks skaitīts no 1 lidz ievadītajam skaitlim ieskaitot, 
+                                # lai pārbaudītu visus iespējamos dalītājus
+        if n % divider == 0: # rezultātu salīdzina ar 0
+                            # lai uzinātu ka reizinās ar konkrēto skaitli
+            n_diveder_count += 1
 
 print(f'Atbilde: {m_diveder_count - n_diveder_count}')
 
@@ -20,3 +26,11 @@ print(f'Atbilde: {m_diveder_count - n_diveder_count}')
 # A2. Izveidot programmu, kas apreiķina naturālu skaitļa, ko ievada lietotājs, 
 # dalāmo skaitu un salīdzina kuram skaitlim ir lielāks dalāmo skaits, izvadot dalāmo skatļu starpību.
 # Programma izveidota: 07.09.2024.
+
+# Ievads      Programmas vēlamā    Rezultāts C++
+#                 reakcija
+
+#  9 6              -1                   +
+#  12 3              4                   +
+#  54 0              8                   +
+#  0 0               0                   +
