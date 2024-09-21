3612098700dc9ff23f1b901f39d62f5f016aeb47
GustavsK <gustavs.krasnikovs@edi.lv>
2024-09-21 10:44:12 +0300

    comlete B4

--- B4.py
+++ B4.py
@@ -1,5 +1,5 @@
 # Gustavs Krasnikovs, gk24018
-#  B4. Noskaidrot, vai dotajā naturālajā skaitlī ir atrodami vienādi cipari. 
+# B4. Noskaidrot, vai dotajā naturālajā skaitlī ir atrodami vienādi cipari. 
 # Ja ir, izdrukāt tos un skaitu, cik reižu dotais cipars ir sastopams. 
 # Skaitļa dalīšana ciparos jāveic skaitliski.
 # Programma izveidota: 17.09.24
@@ -18,4 +18,15 @@ for devider in range(1, x):
 for i in range(len(confirmed_times)):
     print(f'Atrasts vienāds cipars {confirmed_deviders[i]} un cipars ir satopams {confirmed_times[i]}. reizes')
 
-# Testa piemeri
+# Testa plāns
+# Ievads		Programma vēlamā		Rezultāts Python
+# 				   reakcija
+
+#  64				 2 32					+
+#    				 4 16
+# 	    			 8	8
+# 		    		16	4
+# 			    	32	2
+
+#  8		    	 2	4					+
+# 				     4	2
